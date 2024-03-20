c62bbcfa795b226b5a147372a9e5d63e222c8995
Vinicius Perazza <vi_guepardo@hotmail.com>
2024-03-20 15:30:17 -0300

    initial page

--- src/app/(marketing)/header.tsx
+++ src/app/(marketing)/header.tsx
@@ -1,5 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { LoaderIcon } from "lucide-react";
+import Image from "next/image";
+
 export function Header() {
     return (
-        <header className="w-full h-20 px-4 border-b-2 border-slate-200">header</header>
+        <header className="w-full h-20 px-4 border-b-2 border-slate-200">
+            <div className="flex items-center justify-between lg:max-w-screen-lg h-full mx-auto">
+                <div className="flex items-center gap-x-3 pt-8 pl-4 pb-7">
+                    <Image
+                        src='/mascot.svg'
+                        alt="logo"
+                        height={40}
+                        width={40}
+                    />
+                    <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+                        Lingo
+                    </h1>
+                </div>
+
+                <ClerkLoading>
+                    <LoaderIcon className="w-5 h-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton
+                            afterSignOutUrl="/"
+                        />
+                    </SignedIn>
+
+                    <SignedOut>
+                        <SignInButton
+                            mode="modal"
+                            afterSignInUrl="/learn"
+                            afterSignUpUrl="/learn"
+                        >
+                            <Button size='lg' variant="ghost">
+                                Entrar
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
+        </header>
     )
 }
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,63 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { LoaderIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="">
-      <p>Hello World!</p>
-    </main>
+    <div className="flex flex-col lg:flex-row items-center justify-center max-w-[988px] w-full mx-auto p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image
+          src="/hero.svg"
+          alt="Hero image"
+          fill
+        />
+      </div>
+
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="max-w-[480px] text-xl lg:text-3xl text-center font-bold text-neutral-600">
+          Aprenda, pratique, e masterize novas linguas com o Lingo.
+        </h1>
+
+        <div>
+          <ClerkLoading>
+            <LoaderIcon className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button size="lg" variant="secondary" className="w-full">
+                  Começar
+                </Button>
+              </SignUpButton>
+
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  Já tenho uma conta
+                </Button>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">
+                  Continuar Aprendendo
+                </Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
   );
 }
